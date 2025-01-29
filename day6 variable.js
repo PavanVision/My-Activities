781411ceae9b0793fdb0d8e596566154f60f7a60
PavanVision <pavankumarp369@gmail.com>
2025-01-29 22:56:55 +0530

    practice grid cards using bootsrap

---
+++ day6 variable.js
@@ -0,0 +1,12 @@
+let studentname = "john";
+studentname = "mike";
+console.log(studentname);
+
+const birthyear =2000;
+birthyear =2001;
+console.log(birthyear);
+//it is constant so it should be error//
+
+const birthyear =2000;
+birthyear =2001;
+console.log(birthyear);
